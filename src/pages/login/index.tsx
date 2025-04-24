f92aca62c4c89fb53a92fc113adc9b827bedb180
sieon <sieon.eve@gmail.com>
2025-04-25 01:41:08 +0900

    update

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -21,6 +21,7 @@ export function Login() {
         navigate(ROUTE_PATHS.ONBOARDING)
     }
 
+    const [loginerr, setLoginErr] = useState<boolean>(false);
     const [formData, setFormData] = useState({
         email: "",  
         password: "",
@@ -58,15 +59,18 @@ export function Login() {
                     navigate(ROUTE_PATHS.MAIN, { replace: true });
                 } catch (error) {
                     console.error('Failed to fetch user data:', error);
+                    setLoginErr(true);
                     alert('사용자 정보를 가져오는데 실패했습니다.');
                 }
             }
         } catch (error) {
             console.error('Login failed:', error);
+            setLoginErr(true)
             alert('로그인에 실패했습니다.');
         }
     };
-    
+    const isFormValid = formData.email !== '' && formData.password !== '';
+
     const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>, field: keyof PatchAuthLogin) => {
 
         setFormData(prev => {
@@ -103,7 +107,8 @@ export function Login() {
                     />
                 </div>
                 <div style={{display:'flex',width:'100%', flexDirection:'column', alignItems:'center', gap:'5px'}}>
-                    <Button fullWidth onClick={handleLocalLogin}>로그인</Button>
+                    <Button fullWidth onClick={handleLocalLogin} status={isFormValid ? 'active' : 'default'}
+                     state={loginerr ? 'error' : 'default'}>로그인</Button>
                     <div style={{display:'flex',width:'100%', justifyContent:'right', marginRight:'20px', cursor:'pointer', marginTop:'7px'}}>
                         <TextStyled onClick={handleSignup}>회원가입</TextStyled>
                     </div>
